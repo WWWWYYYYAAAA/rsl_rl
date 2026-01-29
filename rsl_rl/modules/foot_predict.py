8d4844fd86e80c9550febc7b3202224ddf5dca12
WWWWYYYYAAAA <767210874@qq.com>
2026-01-29 16:49:55 +0800

    add_obs

--- rsl_rl/modules/foot_predict.py
+++ rsl_rl/modules/foot_predict.py
@@ -35,13 +35,24 @@ class FootPredict(nn.Module):
 
         self.num_obs = 270
         self.lin_vel = 3
-        self.ang_vel = 3
+        self.ang_vel = 0 # get in actor obs
         self.feet_pos_body = 12
         self.feet_vel_body = 12
-        self.elevation_size = 187
-        self.pred_in = self.num_obs + (self.lin_vel + self.ang_vel + self.feet_pos_body + self.feet_vel_body)*history_length+self.elevation_size
+        self.elevation_size = 336
+        self.pred_in = self.num_obs + (self.lin_vel + self.feet_pos_body + self.feet_vel_body)*history_length+self.elevation_size
         self.pred_out = 4+12 # contact flag contact pos
         self.fpred = MLP(self.pred_in, self.pred_out, [1024,512,256], activation)
+        
+    def get_current_data(self, obs;TensorDict):
+        x = self.get_predgroup_obs(obs)
+        cf = self.get_contactflag_obs(obs)
+        foot_pos = self.get_pred_obs(obs)[:,-24:-12]
+        cf_y = cf[:,-4:]
+        #contact
+        foot_pos = foot_pos.view(-1, 4, 3)* cf_y.view(-1, 4, 1)
+        y = torch.cat((cf_y, foot_pos), dim=-1)
+        
+        
 
     def reset(self, dones: torch.Tensor | None = None) -> None:
         pass
@@ -49,30 +60,13 @@ class FootPredict(nn.Module):
     def forward(self) -> None:
         raise NotImplementedError
     
-    @property
-    def action_mean(self) -> torch.Tensor:
-        if hasattr(self, 'distribution') and self.distribution is not None:
-            return self.distribution.mean
-        else:
-            return torch.zeros(self.num_actions)
-        # return self.distribution.mean
-
-    @property
-    def action_std(self) -> torch.Tensor:
-        if self.distribution is None:
-            return torch.ones(self.num_actions) * 0.1  # 小标准差
-        return self.distribution.stddev
-    @property
-    def entropy(self) -> torch.Tensor:
-        if self.distribution is None:
-            return torch.ones(self.num_actions) * 0.1
-        return self.distribution.entropy().sum(dim=-1)
-
-    def evaluate(self, obs: TensorDict, **kwargs: dict[str, Any]) -> torch.Tensor:
-        obs = self.get_critic_obs(obs)
-        obs = self.critic_obs_normalizer(obs)
-        return self.critic(obs)
-
+    def get_data(self):
+        return
+    
+    def get_predgroup_obs(self, obs: TensorDict) -> torch.Tensor:
+        obs_list = [obs[obs_group] for obs_group in self.obs_groups["predgroup"]]
+        return torch.cat(obs_list, dim=-1)
+    
     def get_actor_obs(self, obs: TensorDict) -> torch.Tensor:
         obs_list = [obs[obs_group] for obs_group in self.obs_groups["policy"]]
         return torch.cat(obs_list, dim=-1)
@@ -86,27 +80,19 @@ class FootPredict(nn.Module):
         return torch.cat(obs_list, dim=-1)
     
     def get_contactflag_obs(self, obs: TensorDict) -> torch.Tensor:
-        obs_list = [obs[obs_group] for obs_group in self.obs_groups["cf"]]
+        obs_list = [obs[obs_group] for obs_group in self.obs_groups["cflag"]]
         return torch.cat(obs_list, dim=-1)
     
     def get_pred_obs(self, obs: TensorDict) -> torch.Tensor:
         obs_list = [obs[obs_group] for obs_group in self.obs_groups["pred"]]
         return torch.cat(obs_list, dim=-1)
     
-    def get_footpos_obs(self, obs: TensorDict) -> torch.Tensor:
-        obs_list = [obs[obs_group] for obs_group in self.obs_groups["footpos"]]
+    def get_superem_obs(self, obs: TensorDict) -> torch.Tensor:
+        obs_list = [obs[obs_group] for obs_group in self.obs_groups["superem"]]
         return torch.cat(obs_list, dim=-1)
 
     def get_actions_log_prob(self, actions: torch.Tensor) -> torch.Tensor:
         return self.distribution.log_prob(actions).sum(dim=-1)
-
-    def update_normalization(self, obs: TensorDict) -> None:
-        if self.actor_obs_normalization:
-            actor_obs = self.get_actor_obs(obs)
-            self.actor_obs_normalizer.update(actor_obs)
-        if self.critic_obs_normalization:
-            critic_obs = self.get_critic_obs(obs)
-            self.critic_obs_normalizer.update(critic_obs)
 
     def load_state_dict(self, state_dict: dict, strict: bool = True) -> bool:
         """Load the parameters of the actor-critic model.
